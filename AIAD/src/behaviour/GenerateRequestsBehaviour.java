5471a30761934c4de755f5af92ebd85ab0172a05
VascoUP <vasco_mag_pereira@hotmail.com>
2017-12-04 18:37:38 +0000

    commit commit commit

--- AIAD/src/behaviour/GenerateRequestsBehaviour.java
+++ AIAD/src/behaviour/GenerateRequestsBehaviour.java
@@ -28,6 +28,7 @@ public class GenerateRequestsBehaviour extends CyclicBehaviour {
 		int n = 0;
 		for (int i = bf; i <= tf; i++) {
 			if (randomGenerator.nextInt(building.getFreqOfFloor(i)) == 0) {
+				System.out.println("New request");
 				int floor = i;
 				int d = randomGenerator.nextInt(1);
 				ElevatorDirection direction = ElevatorDirection.values()[d];

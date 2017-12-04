6c7b35525c6223b0759b40538bb5116a3bd2baf5
Diogo Silva <digax96@gmail.com>
2017-12-04 14:58:54 +0000

    GenerateResquests almost done
    There's still a problem with requests because the id must be incremented in the class message imo

--- AIAD/src/behaviour/GenerateRequestsBehaviour.java
+++ AIAD/src/behaviour/GenerateRequestsBehaviour.java
@@ -3,7 +3,10 @@ package behaviour;
 import java.util.Random;
 
 import building.Building;
+import elevator.ElevatorDirection;
 import jade.core.behaviours.CyclicBehaviour;
+import model.Message;
+import model.NewRequest;
 
 public class GenerateRequestsBehaviour extends CyclicBehaviour {
 
@@ -26,6 +29,12 @@ public class GenerateRequestsBehaviour extends CyclicBehaviour {
 		int n = 0;
 		for (int i = bf; i <= tf; i++) {
 			if (randomGenerator.nextInt(building.getFreqOfFloor(i)) == 0) {
+				int id = 0;
+				int floor = i;
+				int d = randomGenerator.nextInt(1);
+				ElevatorDirection direction = ElevatorDirection.values()[d];
+				Message m = new NewRequest(id, floor, direction);
+				this.building.sendMessage(m);
 				n++;
 			}
 		}
